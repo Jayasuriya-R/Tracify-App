48d3d39d758adf071f38b94ca59306efe51110c4
Jayasuriya-R <suryaabi3801@gmail.com>
2025-06-15 22:46:38 +0530

    aligned some css issue

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,10 +7,10 @@ const Table = ({data}) => {
   return (
     <div className="p-4">
       <DataTable value={data} showGridlines tableStyle={{ minWidth: '28rem' }}>
-        <Column field='id' header="ID" sortable style={{ width: '15%' }}/>
-        <Column field='title' header="Name" sortable style={{ width: '20%' }}/>
+        <Column field='id' header="ID" sortable style={{ width: '10%' }}/>
+        <Column field='title' header="Name" sortable style={{ width: '30%' }}/>
         <Column field='description' header="Description" sortable style={{ width: '40%' }} />
-        <Column field='status' header="Staus" sortable style={{ width: '25%' }}/>
+        <Column field='status' header="Staus" sortable style={{ width: '20%' }}/>
         <Column field='createdDate' header="CreatedDate" sortable style={{ width: '25%' }} />
     
       </DataTable>
